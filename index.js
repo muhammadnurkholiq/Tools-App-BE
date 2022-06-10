a81d118c60791c3cc70ee29ae17fb896449f53fa
Nurkholiq <mnurkholiq114@gmail.com>
2022-06-11 00:42:06 +0700

    prepare

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 // cors
 const cors = require("cors");
 
+// router
+const router = require("./src/routes");
+
 // app
 const app = express();
 
@@ -11,6 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/v1/", router);
 
 app.get("/", (req, res) => {
   res.send("Tools App Back End Running on Heroku");
